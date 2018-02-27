68c3088d2710625cc036401ac37ac173c8e5e42d
Gil Barbara <gilbarbara@gmail.com>
2018-02-26 22:00:27 -0300

    improve increased/decreased

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 // @flow
+type TypeInput = string | boolean | number;
+
+function isNumber(...args: any): boolean {
+  return args.every(d => typeof d === 'number');
+}
+
 export default function treeChanges(data: Object, nextData: Object): Object {
   return {
-    changedFrom(key: string, previous: string, actual: string): boolean {
-      return data[key] === previous && nextData[key] === actual;
-    },
-    changedTo(key: string, actual: string): boolean {
-      return data[key] !== actual && nextData[key] === actual;
-    },
     changed(key: string): boolean {
       return data[key] !== nextData[key];
     },
+    changedFrom(key: string, previous: TypeInput, actual: TypeInput): boolean {
+      return data[key] === previous && nextData[key] === actual;
+    },
+    changedTo(key: string, actual: TypeInput): boolean {
+      return data[key] !== actual && nextData[key] === actual;
+    },
     increased(key: string): boolean {
-      return data[key] < nextData[key];
+      return isNumber(data[key], nextData[key]) && data[key] < nextData[key];
     },
     decreased(key: string): boolean {
-      return data[key] > nextData[key];
+      return isNumber(data[key], nextData[key]) && data[key] > nextData[key];
     },
   };
 }
