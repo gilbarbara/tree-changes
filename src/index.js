7416c9c8ca14d55ff8090fcb9aea2f01997c9ed7
Gil Barbara <gilbarbara@gmail.com>
2018-02-26 22:00:50 -0300

    add support for objects and arrays for `changed`
    with deep-diff

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 // @flow
+import deep from 'deep-diff';
+
 type TypeInput = string | boolean | number;
+
+function isPlainObj(...args: any): boolean {
+  return args.every(d => {
+    const prototype = Object.getPrototypeOf(d);
+
+    return toString.call(d)
+      .slice(8, -1) === 'Object' && (prototype === null || prototype === Object.getPrototypeOf({}));
+  });
+}
+
+function isArray(...args: any): boolean {
+  return args.every(d => Array.isArray(d));
+}
 
 function isNumber(...args: any): boolean {
   return args.every(d => typeof d === 'number');
@@ -8,6 +23,12 @@ function isNumber(...args: any): boolean {
 export default function treeChanges(data: Object, nextData: Object): Object {
   return {
     changed(key: string): boolean {
+      if ((isArray(data[key], nextData[key])) || (isPlainObj(data[key], nextData[key]))) {
+        const diff = deep.diff(data[key], nextData[key]);
+
+        return !!diff;
+      }
+
       return data[key] !== nextData[key];
     },
     changedFrom(key: string, previous: TypeInput, actual: TypeInput): boolean {
